f46f671138ce166e9bdc2afca524a37fbfb86871
Sam <31774689+Inillex@users.noreply.github.com>
2020-07-22 15:28:43 -0600

    Return result

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function fn1() {
 	var dogAge = document.getElementById("age").value;
 	if (dogAge => 1) {
-		var dogAgeToHuman = (16 * (Math.LN2 * dogAge)) + 31;
-		alert("Human age is: " + dogAgeToHuman);
+		var dogAgeToHuman = (16 * (Math.log(dogAge))) + 31;
+		document.getElementById("result").innerHTML = dogAgeToHuman;
 	} else {
 		return;
 	}
