29fb624883831ff655e4f0a2338d51eebf0ebabe
Sam <31774689+Inillex@users.noreply.github.com>
2020-07-22 15:43:22 -0600

    Rounded result

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function fn1() {
 	var dogAge = document.getElementById("age").value;
 	if (dogAge => 1) {
-		var dogAgeToHuman = (16 * (Math.log(dogAge))) + 31;
+		var dogAgeToHuman = (16 * Math.log(dogAge)) + 31;
+		dogAgeToHuman = dogAgeToHuman.toFixed(2);
 		document.getElementById("result").innerHTML = dogAgeToHuman;
 	} else {
 		return;
